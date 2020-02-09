85aa43e845273034a65265c3415cbf6d36a7c615
Lutfor Rahman <contact.lutforrahman@gmail.com>
2020-02-09 16:37:28 +0600

    Removed the repo

--- android/src/main/java/com/github/contactlutforrahman/flutter_qr_bar_scanner/NoPermissionException.java
+++
@@ -1,4 +0,0 @@
-package com.github.contactlutforrahman.flutter_qr_bar_scanner;
-
-class NoPermissionException extends RuntimeException {
-}
--- android/src/main/java/com/github/contactlutforrahman/flutter_qr_bar_scanner/QrReaderCallbacks.java
+++
@@ -1,4 +0,0 @@
-package com.github.contactlutforrahman.flutter_qr_bar_scanner;
-public interface QrReaderCallbacks {
-    void qrRead(String data);
-}
